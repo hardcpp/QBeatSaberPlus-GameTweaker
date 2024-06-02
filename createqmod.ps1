dca79df555ac874ece2cf5347a10d31dc38524ca
HardCPP <hardcpp@gmail.com>
2024-06-02 19:44:12 +0200

    Bump version 6.3.0

--- createqmod.ps1
+++ createqmod.ps1
@@ -32,13 +32,19 @@ foreach ($mod in $modJson.modFiles) {
     }
     $filelist += $path
 }
+foreach ($mod in $modJson.lateModFiles) {
+    $path = "./build/" + $mod
+    if (-not (Test-Path $path)) {
+        $path = "./extern/libs/" + $mod
+    }
+    if (-not (Test-Path $path)) {
+        Write-Output "Error: could not find dependency: $path"
+        exit 1
+    }
+    $filelist += $path
+}
 
 foreach ($lib in $modJson.libraryFiles) {
-    #if ($lib -eq "libchatplex-sdk-bs.so") {
-    #    Continue
-    #}
-
-
     $path = "./build/" + $lib
     if (-not (Test-Path $path)) {
         $path = "./extern/libs/" + $lib
